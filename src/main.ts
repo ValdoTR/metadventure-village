a903d99ef39e49e4b42ee001d8f5107906ace05c
Valdo <vteixeiraromao@gmail.com>
2022-10-11 15:45:14 +0100

    Added Llamaverse and FestAdventure exits

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,50 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
+let currentPopup: any = undefined;
+
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
-    WA.room.onEnterLayer('officeZone').subscribe(() => {
+    // Teleport
+    WA.room.area.onEnter("Llamaverse").subscribe(() => {
+        currentPopup = WA.ui.openPopup("LlamaversePopup", "Visit Llamaverse", [
+            {
+                label: 'Teleport',
+                className: 'primary',
+                callback: () => WA.nav.goToRoom("/@/llamaverse"),
+            }
+        ])
+    })
+    WA.room.area.onLeave("Llamaverse").subscribe(() => {closePopup(); WA.nav.closeCoWebSite()})
+
+    WA.room.area.onEnter("FestAdventure").subscribe(() => {
+        currentPopup = WA.ui.openPopup("FestAdventurePopup", "Visit FestAdventure", [
+            {
+                label: 'Teleport',
+                className: 'primary',
+                callback: () => WA.nav.goToRoom("/@/festadventure"),
+            }
+        ])
+    })
+    WA.room.area.onLeave("FestAdventure").subscribe(() => {closePopup(); WA.nav.closeCoWebSite()})
+
+    WA.room.area.onEnter("ComingSoon").subscribe(() => {
+        currentPopup = WA.ui.openPopup("ComingSoonPopup", "Coming Soon!", [])
+    })
+    WA.room.area.onLeave("ComingSoon").subscribe(() => {closePopup(); WA.nav.closeCoWebSite()})
+
+    // Roofs
+    WA.room.area.onEnter("Office").subscribe(() => {
         WA.room.hideLayer("roofOffice0");
         WA.room.hideLayer("roofOffice1");
         WA.room.hideLayer("roofOffice2");
 
         WA.room.showLayer("roofOfficeOverlay");
-    })      
-
-    WA.room.onLeaveLayer('officeZone').subscribe(() => {
+    })
+    WA.room.area.onLeave("Office").subscribe(() => {
         WA.room.showLayer("roofOffice0");
         WA.room.showLayer("roofOffice1");
         WA.room.showLayer("roofOffice2");
@@ -25,16 +55,14 @@ WA.onInit().then(() => {
         WA.room.hideLayer("roofOfficeOverlay");
     })
 
-
-    WA.room.onEnterLayer('nftZone').subscribe(() => {
+    WA.room.area.onEnter("NFTs").subscribe(() => {
         WA.room.hideLayer("roofNFT0");
         WA.room.hideLayer("roofNFT1");
         WA.room.hideLayer("roofNFT2");
 
         WA.room.showLayer("roofNFTOverlay");
-    })      
-
-    WA.room.onLeaveLayer('nftZone').subscribe(() => {
+    })
+    WA.room.area.onLeave("NFTs").subscribe(() => {
         WA.room.showLayer("roofNFT0");
         WA.room.showLayer("roofNFT1");
         WA.room.showLayer("roofNFT2");
@@ -49,5 +77,11 @@ WA.onInit().then(() => {
 
 }).catch(e => console.error(e));
 
+function closePopup(){
+    if (currentPopup !== undefined) {
+        currentPopup.close()
+        currentPopup = undefined
+    }
+}
 
 export {};
